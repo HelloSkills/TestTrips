392a38bd9f3035c449639667ec381a58ad406b0c
HelloSkills <HelloSkillsWeb@gmail.com>
2025-08-25 13:28:11 +0300

    fix(useJsonServer): delete useJsonServer

--- app/frontend/src/composables/useJsonServer.ts
+++ app/frontend/src/composables/useJsonServer.ts
@@ -7,23 +7,17 @@ export const users = ref([])
 export const aviaVariants = ref([])
 export const trips = ref([])
 
+// Получаем всех юзеров
 export const getUsers = async () => {
     users.value = (await axios.get(`${BACKEND_URL}/users`)).data
 }
 
+// Получаем все услуги
 export const getAviaVariants = async () => {
     aviaVariants.value = (await axios.get(`${BACKEND_URL}/aviaVariants`)).data
 }
 
+// Получаем все путёвки
 export const getTrips = async () => {
     trips.value = (await axios.get(`${BACKEND_URL}/trips`)).data
 }
-
-export const useJsonServer = () => ({
-    users,
-    aviaVariants,
-    trips,
-    getUsers,
-    getAviaVariants,
-    getTrips,
-})
