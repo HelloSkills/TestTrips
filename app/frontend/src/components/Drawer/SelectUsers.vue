ebe63f684b776ef30544fc04432bed5eb4edbc2a
HelloSkills <HelloSkillsWeb@gmail.com>
2025-08-27 15:18:00 +0300

    fix(SelectUsers): fix users

---
+++ app/frontend/src/components/Drawer/SelectUsers.vue
@@ -0,0 +1,124 @@
+<template>
+  <div :class="$style.container">
+    <div :class="$style.select">
+      <div :class="$style.users" @click="toggleDropdown">
+        Выберите сотрудников
+      </div>
+      <img src="/icons/arrow_down.svg" alt="arrow_down_icon" width="20" height="20" />
+    </div>
+    <div :class="$style.selectedUsers" v-for="user in users" :key="user.id">
+      <div :class="$style.userName">
+        {{ user.second_name }} {{ user.last_name }}
+      </div>
+      <img :class="$style.icon" @click="deleteUser" src="/icons/delete.svg" alt="delete_icon" width="16" height="16" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const users = ref([
+  {
+    "id": "1",
+    "first_name": "Смирнов",
+    "second_name": "Алексей",
+    "last_name": "Сергеевич"
+  },
+  {
+    "id": "2",
+    "first_name": "Кузнецова",
+    "second_name": "Анна",
+    "last_name": "Дмитриевна"
+  },
+  {
+    "id": "3",
+    "first_name": "Попов",
+    "second_name": "Михаил",
+    "last_name": "Андреевич"
+  },
+  {
+    "id": "4",
+    "first_name": "Соколова",
+    "second_name": "Екатерина",
+    "last_name": "Владимировна"
+  },
+  {
+    "id": "5",
+    "first_name": "Лебедев",
+    "second_name": "Никита",
+    "last_name": "Александрович"
+  },
+  {
+    "id": "6",
+    "first_name": "Морозова",
+    "second_name": "Ольга",
+    "last_name": "Сергеевна"
+  },
+  {
+    "id": "7",
+    "first_name": "Новиков",
+    "second_name": "Дмитрий",
+    "last_name": "Игоревич"
+  },
+  {
+    "id": "8",
+    "first_name": "Федорова",
+    "second_name": "Светлана",
+    "last_name": "Викторовна"
+  },
+  {
+    "id": "9",
+    "first_name": "Михайлов",
+    "second_name": "Владимир",
+    "last_name": "Петрович"
+  },
+  {
+    "id": "10",
+    "first_name": "Алексеева",
+    "second_name": "Марина",
+    "last_name": "Александровна"
+  }
+  ])
+
+const deleteUser = () => {
+  console.log('Удаляем юзера')
+}
+</script>
+
+<style lang="scss" module>
+.container {
+  width: 100%;
+  cursor: pointer;
+}
+
+.select {
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 10px 12px 15px;
+  margin: 20px;
+  background-color: #FFFFFF;
+  border-radius: 5px;
+}
+
+.selectedUsers {
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 20px;
+  padding-left: 15px;
+  background-color: #FFFFFF;
+  position: relative;
+}
+
+.icon {
+  position: absolute;
+  top: -7px;
+  right: 5px;
+  cursor: pointer;
+}
+</style>
--- app/frontend/src/components/Drawer/index.vue
+++ app/frontend/src/components/Drawer/index.vue
@@ -1,25 +1,34 @@
 <template>
   <div :class="$style.container">
-    <div :class="$style.title">
-      <div :class="$style.create">
-        Создание поездки
+    <div :class="$style.wrap">
+      <div :class="$style.title">
+        <div :class="$style.create">
+          Создание поездки
+        </div>
+        <img :class="$style.close" src="/icons/close.svg" alt="close_icon">
       </div>
-      <img src="/icons/close.svg" alt="close_icon">
+      <div :class="$style.name">
+        <input
+            v-model="searchQuery"
+            type="text"
+            placeholder="Введите название поездки"
+            :class="$style.inputSearch"
+            @keyup.enter="search"
+        >
+      </div>
+      <SelectUsers/>
     </div>
-    <div :class="$style.name">
-      <input
-          v-model="searchQuery"
-          type="text"
-          placeholder="Введите название поездки"
-          :class="$style.inputSearch"
-          @keyup.enter="search"
-      >
+    <div :class="$style.createTrip">
+      <div :class="$style.createBtn">
+        Создать
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import SelectUsers from "@/components/Drawer/SelectUsers.vue";
 const searchQuery = ref('')
 const search = () => {
   console.log('searchQuery.value', searchQuery.value)
@@ -33,6 +42,9 @@ const search = () => {
   height: 855px; // потом уберём
   background-color: #F6F8FC;
   border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
 .title {
@@ -43,6 +55,7 @@ const search = () => {
   justify-content: space-between;
   align-items: center;
   background: #FFFFFF;
+  border-bottom: 1px solid #B3C1D1;
 }
 
 .create {
@@ -61,7 +74,8 @@ const search = () => {
 .inputSearch {
   all: unset;
   border-bottom: 1px solid transparent;
-  width: 180px;
+  width: 174px;
+  color: black;
 
   &::placeholder {
     color: #9ca3af;
@@ -72,7 +86,30 @@ const search = () => {
 
   &:focus {
     border-bottom: 1px solid #4361EE;
-    color: black;
   }
+}
+
+.inputSearch, .close {
+  cursor: pointer;
+}
+
+.createTrip {
+  border-top: 1px solid #B3C1D1;
+  height: 60px;
+  background-color: #FFFFFF;
+  display: flex;
+  align-items: center;
+}
+
+.createBtn {
+  width: 150px;
+  height: 40px;
+  margin-left: 20px;
+  padding: 12px 47px;
+  background-color: #4361EE;
+  color: #FFFFFF;
+  font-size: 14px;
+  line-height: 14px;
+  border-radius: 5px;
 }
 </style>
