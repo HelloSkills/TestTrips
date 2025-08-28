47d2ae8f6e4776371b5166b69ed209ce4e271020
HelloSkills <HelloSkillsWeb@gmail.com>
2025-08-28 15:39:21 +0300

    feat(vue-datepicker): i and style vue-datepicker in SelectAvia.vue

--- app/frontend/src/components/Avia/SelectAvia.vue
+++ app/frontend/src/components/Avia/SelectAvia.vue
@@ -1,13 +1,100 @@
 <template>
-  <div :class="$style.cointainer">
-
+  <div :class="$style.container">
+    <div :class="$style.inputsAll">
+      <div :class="$style.placeFrom">
+        <img src="/icons/air.svg" alt="air_icon" width="20" height="20">
+        <input :class="$style.input" type="text" placeholder="Город вылета" />
+      </div>
+      <div :class="$style.placeTo">
+        <img src="/icons/air.svg" alt="air_icon" width="20" height="20">
+        <input :class="$style.input" type="text" placeholder="Город прилёта" />
+      </div>
+      <div :class="$style.timeFrom">
+        <img src="/icons/calendar.svg" alt="calendar_icon" width="20" height="20">
+        <Datepicker
+            v-model="dateFrom"
+            placeholder="Туда"
+            :enable-time-picker="false"
+            :hide-input-icon="true"
+        />
+      </div>
+      <div :class="$style.timerTo">
+        <img src="/icons/calendar.svg" alt="calendar_icon" width="20" height="20">
+        <Datepicker
+            v-model="dateFrom"
+            placeholder="Обратно"
+            :enable-time-picker="false"
+            :hide-input-icon="true"
+        />
+      </div>
+    </div>
+    <div :class="$style.searchBtn">
+      <div :class="$style.search">Найти</div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import Datepicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+import { ref } from 'vue'
 
+const dateFrom = ref(null)
+const dateTo = ref(null)
 </script>
 
 <style lang="scss" module>
+.container {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  background-color: #FFFFFF;
+  height: 140px;
+  padding: 20px;
+}
 
+.inputsAll {
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  height: 40px;
+}
+
+.input {
+  all: unset;
+  height: 40px;
+  cursor: pointer;
+}
+
+.placeFrom, .placeTo, .timeFrom, .timerTo {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+  background-color: #F6F8FC;
+  border-radius: 5px;
+  padding: 10px;
+}
+
+.searchBtn {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.search {
+  background-color: #4361EE;
+  height: 40px;
+  color: #FFFFFF;
+  font-size: 14px;
+  line-height: 14px;
+  padding: 12px 70px;
+  border-radius: 5px;
+}
+
+:global(.dp__input) {
+  all: unset;
+  height: 40px;
+  cursor: pointer;
+}
 </style>
--- app/frontend/src/components/Avia/index.vue
+++ app/frontend/src/components/Avia/index.vue
@@ -1,17 +1,19 @@
 <template>
   <div :class="$style.container">
     <SelectUsers :isAvia="true" :class="aviaSelect"/>
+    <SelectAvia/>
   </div>
 </template>
 
 <script setup lang="ts">
 
 import SelectUsers from "@/components/Drawer/SelectUsers.vue"
+import SelectAvia from "@/components/Avia/SelectAvia.vue"
 </script>
 
 <style lang="scss" module>
 .container {
-  width: 840px;
+  width: max-content;
   margin: 58px auto 0;
 }
 
